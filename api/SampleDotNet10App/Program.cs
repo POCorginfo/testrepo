98506263eec41e10dbb365b5847d93f81010403a
qxz6bo6 <sagrawal@ventum.de>
2026-01-19 17:03:20 +0530

    changes

--- api/SampleDotNet10App/Program.cs
+++ api/SampleDotNet10App/Program.cs
@@ -18,11 +18,6 @@ string result = number switch
     > 50 => "Greater than 50"
 };
 //comment for test-1.0.1
-//comment for test-1.0.2
-//comment for test-1.0.3
-//comment for test-1.0.4
-//comment for test-1.0.5
-//comment for test-1.0.6
 Console.WriteLine($"Number1: {number} → {result}");
 Console.WriteLine($"Number: {number} → {result}");
 Console.WriteLine($"Number2: {number} → {result}");
